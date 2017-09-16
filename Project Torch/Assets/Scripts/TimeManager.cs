0fbe71a5553317be78ddcfd55fbe54241c15ea71
Connor Menard <cjm5772@rit.edu>
2017-09-16 16:28:54 -0400

    Basic hour timer
    Counts down but doesn't do anything else yet

---
+++ Project Torch/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour {
+
+    //Public so that you can watch it in the inspector
+    public float torchTime;
+
+	void Start () {
+        torchTime = 3600.000f; //1 hour in seconds
+    }
+	
+	void Update () {
+        torchTime -= Time.deltaTime;
+	}
+}
