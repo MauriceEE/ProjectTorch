498ba0df143f37e5ece2d7953e0e606299f64a4b
Connor Menard <cjm5772@rit.edu>
2017-10-21 22:39:22 -0400

    Added zone transitions and zone end point scripts

---
+++ Project Torch/Assets/Scripts/ZoneEndPoint.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// At the moment this exists only to hold data about level transitions
+/// </summary>
+public class ZoneEndPoint : MonoBehaviour {
+
+#region Public Fields
+    //Zone to warp the player to
+    public ZoneManager.Zones nextZone;
+    //New spot for the player to arrive at,
+    //only X because we want to keep their Y the same
+    public float newXCoordinate;
+#endregion
+}
--- Project Torch/Assets/Scripts/ZoneManager.cs
+++ Project Torch/Assets/Scripts/ZoneManager.cs
@@ -7,6 +7,7 @@ using UnityEngine.PostProcessing;
 /// </summary>
 public class ZoneManager : MonoBehaviour {
     #region Enums
+    //All possible zones for the player to be in
     public enum Zones
     {
         Battlefield,
@@ -15,35 +16,213 @@ public class ZoneManager : MonoBehaviour {
         CastleOfMan,
         FortressOfDark
     }
+    //Used to keep track of how to manipulate the black screen obj
+    protected enum TransitionPhase
+    {
+        NONE,
+        FadingOut,
+        FadingIn
+    }
     #endregion
 
     #region Public Fields
+    //Current zone the player is in
     public Zones currentZone;
+    //Reference to the black screen object for scene transitions
+    public GameObject blackScreen;
+    //Reference to all the zone transition objects
+    [Header("End Points")]
+    public GameObject battlefieldLeftEndPoint;
+    public GameObject battlefieldRightEndPoint;
+    public GameObject sullenVillageLeftEndPoint;
+    public GameObject sullenVillageRightEndPoint;
+    public GameObject thrivingVillageLeftEndPoint;
+    public GameObject thrivingVillageRightEndPoint;
+    public GameObject throneRoomEndPoint;
+    public GameObject fortressKeepEndPoint;
+    //Reference to all the parent zone objects
+    [Header("Zone Parents")]
+    public GameObject battlefieldZone;
+    public GameObject sullenVillageZone;
+    public GameObject thrivingVillageZone;
+    public GameObject throneRoomZone;
+    public GameObject fortressKeepZone;
     #endregion
 
     #region Private Fields
-    private Camera gameCamera;
-    private PostProcessChange profileChanger;
+    //Reference to the camera's post process changer
+    protected PostProcessChange profileChanger;
+    //The material of the black screen, used for fading
+    protected Material screenFade;
+    //The current phase of transitioning
+    protected TransitionPhase phase;
+    //The player
+    protected GameObject player;
+
+    protected float cameraMin, cameraMax;
+
+    protected Zones nextZone;
+    protected float newPlayerXCoordinate;
     #endregion
 
     #region Unity Defaults
     void Start () {
-        currentZone = Zones.Battlefield;
-        gameCamera = Camera.main;
-        profileChanger = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PostProcessChange>();
-	}
+        profileChanger = Camera.main.GetComponent<PostProcessChange>();
+        screenFade = blackScreen.GetComponent<Renderer>().material;
+        player = GameObject.Find("Player");
+        //Assign camera clamp values
+        UpdateCameraClamp();
+        //Make black screen transparent at first
+        blackScreen.SetActive(true);
+        Color fadeColor = screenFade.color;
+        fadeColor.a = 0f;
+        screenFade.color = fadeColor;
+    }
 	
 	void Update () {
-		if(currentZone == Zones.ThrivingVillage || currentZone == Zones.FortressOfDark)
+        //Set darkness to true if in the thriving village or fortress of dark
+        profileChanger.darkness = (currentZone == Zones.ThrivingVillage || currentZone == Zones.FortressOfDark);
+        //Keep the camera within the bounds
+        ClampCamera();
+        //Check to see if within range of a zone end
+        switch(phase)
         {
-            // set darkness to true
-            profileChanger.darkness = true;
+            case TransitionPhase.NONE:
+                switch (currentZone)
+                {
+                    case Zones.Battlefield:
+                        if (player.transform.position.x <= battlefieldLeftEndPoint.transform.position.x)
+                        {
+                            ZoneEndPoint endpoint = battlefieldLeftEndPoint.GetComponent<ZoneEndPoint>();
+                            ChangeZone(endpoint.nextZone, endpoint.newXCoordinate);
+                        }
+                        else if (player.transform.position.x >= battlefieldRightEndPoint.transform.position.x)
+                        {
+                            ZoneEndPoint endpoint = battlefieldRightEndPoint.GetComponent<ZoneEndPoint>();
+                            ChangeZone(endpoint.nextZone, endpoint.newXCoordinate);
+                        }
+                        break;
+                    case Zones.SullenVillage:
+                        break;
+                    case Zones.ThrivingVillage:
+                        break;
+                    case Zones.CastleOfMan:
+                        break;
+                    case Zones.FortressOfDark:
+                        break;
+                }
+                break;
+            case TransitionPhase.FadingIn:
+                FadeIn();
+                break;
+            case TransitionPhase.FadingOut:
+                FadeOut();
+                break;
+        }
+        
+	}
+    #endregion
+
+    #region Custom Methods
+    protected void ChangeZone(Zones _nextZone, float newXCoordinate)
+    {
+        phase = TransitionPhase.FadingOut;
+        nextZone = _nextZone;
+        newPlayerXCoordinate = newXCoordinate;
+    }
+
+    protected void ClampCamera()
+    {
+        if (Camera.main.transform.position.x < cameraMin)
+            Camera.main.transform.position = new Vector3(cameraMin, Camera.main.transform.position.y, Camera.main.transform.position.z);
+        else if (Camera.main.transform.position.x > cameraMax)
+            Camera.main.transform.position = new Vector3(cameraMax, Camera.main.transform.position.y, Camera.main.transform.position.z);
+    }
+
+    protected void UpdateCameraClamp()
+    {
+        switch (currentZone)
+        {
+            case Zones.Battlefield:
+                cameraMin = battlefieldLeftEndPoint.transform.position.x;
+                cameraMax = battlefieldRightEndPoint.transform.position.x;
+                break;
+            case Zones.SullenVillage:
+                cameraMin = sullenVillageLeftEndPoint.transform.position.x;
+                cameraMax = sullenVillageRightEndPoint.transform.position.x;
+                break;
+            case Zones.ThrivingVillage:
+                cameraMin = thrivingVillageLeftEndPoint.transform.position.x;
+                cameraMax = thrivingVillageRightEndPoint.transform.position.x;
+                break;
+            case Zones.CastleOfMan:
+                cameraMin = 0f;
+                cameraMax = 0f;
+                break;
+            case Zones.FortressOfDark:
+                cameraMin = 0f;
+                cameraMax = 0f;
+                break;
+        }
+    }
+
+    protected void SetZoneActive(Zones zone, bool active)
+    {
+        switch (zone)
+        {
+            case Zones.Battlefield:
+                battlefieldZone.SetActive(active);
+                break;
+            case Zones.SullenVillage:
+                sullenVillageZone.SetActive(active);
+                break;
+            case Zones.ThrivingVillage:
+                thrivingVillageZone.SetActive(active);
+                break;
+            case Zones.CastleOfMan:
+                throneRoomZone.SetActive(active);
+                break;
+            case Zones.FortressOfDark:
+                fortressKeepZone.SetActive(active);
+                break;
+        }
+    }
+
+    protected void FadeOut()
+    {
+        if (screenFade.color.a < 1)
+        {
+            Color fadeColor = screenFade.color;
+            fadeColor.a += Time.deltaTime;
+            screenFade.color = fadeColor;
         }
         else
         {
-            // set darkness to false
-            profileChanger.darkness = false;
+            //If we get here, the screen is completely black, so...
+            //Deactivate this zone
+            SetZoneActive(currentZone, false);
+            //Activate the next one
+            SetZoneActive(nextZone, true);
+            //Update current zone
+            currentZone = nextZone;
+            //Update the camera min/max
+            UpdateCameraClamp();
+            //Update player's position
+            player.transform.position = new Vector3(newPlayerXCoordinate, player.transform.position.y, player.transform.position.z);
+            //Move to fade in phase
+            phase = TransitionPhase.FadingIn;
         }
-	}
+    }
+
+    protected void FadeIn()
+    {
+        if (screenFade.color.a > 0)
+        {
+            Color fadeColor = screenFade.color;
+            fadeColor.a -= Time.deltaTime;
+            screenFade.color = fadeColor;
+        }
+        else phase = TransitionPhase.NONE;
+    }
 #endregion
 }
