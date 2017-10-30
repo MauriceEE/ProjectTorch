e05a5e20b5aebdd2e37803f5df6324c01a24231f
Joey Tong <joeyptong@yahoo.com>
2017-10-30 19:23:47 -0400

    UI Sounds

--- Project Torch/Assets/Scripts/UI/UIManager.cs
+++ Project Torch/Assets/Scripts/UI/UIManager.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 public class UIManager : MonoBehaviour {
     GameObject menuCanvas;
+    public List<Button> buttons;
     // Use this for initialization
     void Start () {
         menuCanvas = gameObject.transform.GetChild(0).gameObject;
-
+        buttons = new List<Button>();
     }
 	
 	// Update is called once per frame
@@ -19,7 +21,13 @@ public class UIManager : MonoBehaviour {
         if (SceneManager.sceneCount > 1 && SceneManager.GetSceneAt(1).isLoaded) {
             SceneManager.UnloadSceneAsync("Loading");
         }
-
+       
+       /* foreach(Button b in buttons) {
+            if (EventSystem.current.IsPointerOverGameObject()) {
+                AkSoundEngine.PostEvent("Scroll",gameObject);
+                Debug.Log("Sound");
+            }
+        }*/
     }
     public void Resume() {
         menuCanvas.SetActive(true);
---
+++ Project Torch/Assets/Scripts/UI/UISounds.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISounds : MonoBehaviour, IPointerEnterHandler {
+    public void OnPointerEnter(PointerEventData eventData) {
+        AkSoundEngine.PostEvent("Pause", gameObject);
+        Debug.Log("Sound");
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
