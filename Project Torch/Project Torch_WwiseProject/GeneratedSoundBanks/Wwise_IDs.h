cf73234a48dd7debb9ba56ea13416d8f47adacf5
Garret Reynolds <reynolds.garret@gmail.com>
2017-10-30 18:44:27 -0400

    Wwise Updated Soundbanks
    Generated Soundbanks for last Commit

---
+++ Project Torch/Project Torch_WwiseProject/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,75 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ATTACK1 = 3768540966U;
+        static const AkUniqueID BUTTONPRESS = 317641954U;
+        static const AkUniqueID DASH = 1942692385U;
+        static const AkUniqueID PAUSE = 3092587493U;
+        static const AkUniqueID RESUME = 953277036U;
+        static const AkUniqueID SCROLL = 454121546U;
+        static const AkUniqueID SHINE = 3055381918U;
+        static const AkUniqueID SLASH = 4107276880U;
+        static const AkUniqueID THRUST = 2197345151U;
+        static const AkUniqueID TORCH_PLANT = 2547765723U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMESTATE
+        {
+            static const AkUniqueID GROUP = 4091656514U;
+
+            namespace STATE
+            {
+                static const AkUniqueID INGAME = 984691642U;
+                static const AkUniqueID PAUSE = 3092587493U;
+            } // namespace STATE
+        } // namespace GAMESTATE
+
+        namespace PLAYERLIFE
+        {
+            static const AkUniqueID GROUP = 444815956U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ALIVE = 655265632U;
+                static const AkUniqueID DEAD = 2044049779U;
+            } // namespace STATE
+        } // namespace PLAYERLIFE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID PLAYERHEALTH = 151362964U;
+        static const AkUniqueID TIMEREMAINING = 1691631674U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
