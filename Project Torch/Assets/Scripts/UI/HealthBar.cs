5a571254dc7ee07fad6225e3bcc425f6f684211f
Joey Tong <joeyptong@yahoo.com>
2017-10-31 14:10:42 -0400

    Splash Screen & health bar

---
+++ Project Torch/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+    public PlayerCombat player;
+    public RectTransform health;
+    public RectTransform bar;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        health.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, player.hp);
+            
+	}
+}
